854c241fe2abeb690b3ff37fd4153ab0e924130e
Rohitttkr <106422915+Rohitttkr@users.noreply.github.com>
2026-02-04 18:33:11 +0530

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import get_song_link
+from .views import get_song_link  # Views file se function ko import kiya
 
 urlpatterns = [
-    path('search/', get_song_link, name='search_song'),
+    # Jab koi 'https://tumhara-app.onrender.com/api/search/' par request bhejega
+    # toh ye niche wala path trigger hoga aur get_song_link function chalega.
+    
+    path('search/', get_song_link, name='get_song_link'),
 ]
